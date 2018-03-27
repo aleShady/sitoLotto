8bdc54e647a18d2306ce90ea6afd35738bd8feb1
Ale <siverinoa@gmail.com>
2018-03-27 10:08:28 +0200

    reata sezione per sestines

---
+++ App/quadratureTest/query.php
@@ -0,0 +1,31 @@
+<?php
+include '../../Classes/DBM.php';
+	include '../../Classes/Quadrature.php';
+$db = new DBM();
+$myYear =1871;
+while($myYear <= 2020){
+     $db->write("ALTER TABLE sest$myYear add composizione varchar(255)");
+    $myYear++;
+    
+}
+    
+//
+//CREATE TABLE Employee(
+//Employee_Id CHAR(12)NOT NULL PRIMARY KEY,
+//First_name CHAR(30),
+//Last_name CHAR(30),
+//Address VARCHAR(50),
+//City CHAR,
+//State CHAR,
+//Salary INT,
+//Gender CHAR,
+//Age INT
+//);
+//
+//DROP TABLE Job;
+//
+//CREATE TABLE job(
+//Exempt_Non_Exempt_Status tinyint(1) NOT NULL PRIMARY KEY,
+//Job_title CHAR,
+//Job_description CHAR
+//); 
